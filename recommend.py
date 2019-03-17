90a503944bf7769ee15fa3de435f7b68cbeb7434
Binderiya Adishaa <b.adishaa@mail.utoronto.ca>
2019-03-17 08:25:50 -0400

    fixed bug

--- recommend.py
+++ recommend.py
@@ -1,11 +1,13 @@
 import numpy as np
-
+import pandas as pd
 if __name__ == '__main__':
 
     sparse = np.load('sparse_matrix.npy')
     full_m = np.load('full_matrix.npy')
     u_user_id = np.load('u_user_id.npy')
     u_book_id = np.load('u_book_id.npy')
+    b_data = pd.read_csv('books.csv')[['id', 'title']]
+    b_data['title'] = b_data['title']
 
     def u_map(x):
         return np.where(u_user_id == x)[0][0]
@@ -21,19 +23,16 @@ if __name__ == '__main__':
         return list(b_data[b_data['id'] == book_id]['title'])
 
     run = True
-    user_id = input('Enter user_id:')
+    
     while run:
-    	if user_id in u_user_id:
-    		print(recommend(user_id, full_m, sparse))
-    		response = input('Run another query (y/n)?')
-    		while response not in ['y', 'n']:
-    			response = input('Please input y or n:')
-    		if response == 'n':
-    			run = False
-    	else:
-    		response = input('Please input valid user_id:')
-    		while user_id not in u_user_id:
-    			response = input('Please input valid user_id:')
+        user_id = int(input('Please input valid user_id:'))
+        if user_id in u_user_id:
+            print(recommend(user_id, full_m, sparse))
+            response = input('Run another query (y/n)?')
+            while response not in ['y', 'n']:
+                response = input('Please input y or n:')
+            if response == 'n':
+                run = False
     		
 
 
